720f505ee5846a3b99a72f4ed65e0bb44590e616
Aakash Prakash <aakash.prakash@accoliteindia.com>
2019-02-26 19:27:18 +0530

    Review comment fixes for #7

--- lib/froala-editor-sdk/image.rb
+++ lib/froala-editor-sdk/image.rb
@@ -10,20 +10,22 @@ module FroalaEditorSDK
             allowedMimeTypes: [ "image/gif", "image/jpeg", "image/pjpeg", "image/x-png", "image/png", "image/svg+xml" ]
         },
         resize: nil,
-        file_upload_path: 'public/uploads/images',
         file_access_path: '/uploads/'
     }
+
+    # Default upload path.
+    @default_upload_path = "public/uploads/images"
 
     # Loads the images from a specific path
     # Params:
     # +path+:: The server path where the images are saved
     # Returns Json object
-    def self.load_images(options = {})
+    def self.load_images(path, options = {})
 
       # Merge options.
       options = @default_options.merge(options)
 
-      images = Dir["#{options[:file_upload_path]}*"]
+      images = Dir["#{path}*"]
       all_images = []
 
       images.each do |img|
